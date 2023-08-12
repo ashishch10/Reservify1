ddce775357e21a6cbf1a691f60bbe1bf7e4b8c69
Ashish <ashishgartan03@gmail.com>
2023-08-12 23:11:55 +0530

    initial commit

---
+++ api/models/Booking.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema({
+  place: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Place" },
+  user: { type: mongoose.Schema.Types.ObjectId, required: true },
+  checkIn: { type: Date, required: true },
+  checkOut: { type: Date, required: true },
+  name: { type: String, required: true },
+  phone: {
+    type: String,
+    required: true,
+    validate: function () {
+      return this.phone.length == 10;
+    },
+  },
+  price: Number,
+});
+
+const BookingModel = mongoose.model("Booking", bookingSchema);
+
+module.exports = BookingModel;
